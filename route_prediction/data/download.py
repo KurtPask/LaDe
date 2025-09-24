7e359116a0920fd28ea30787f9c2f386ef6e21ca
KurtPask <kurtpasque@gmail.com>
2025-09-24 16:56:31 -0700

    changed download.py to have both delivery and pickup

--- route_prediction/data/download.py
+++ route_prediction/data/download.py
@@ -1,7 +1,9 @@
 from datasets import load_dataset
 
 splits = ['jl', 'cq', 'yt', 'sh', 'hz']
-type_dataset = 'delivery_'
 for s in splits:
-    ds = load_dataset("Cainiao-AI/LaDe-D", split=f"{type_dataset}{s}")
-    ds.to_csv(f"{type_dataset}{s}.csv")
+    ds = load_dataset("Cainiao-AI/LaDe-D", split=f"delivery_{s}")
+    ds.to_csv(f"delivery_{s}.csv")
+
+    ds = load_dataset("Cainiao-AI/LaDe-P", split=f"pickup_{s}")
+    ds.to_csv(f"pickup_{s}.csv")
